4435e1e89c5ebf79008f23a08e319a70b85fc2a5
oh-livia <oaw2102@columbia.edu>
2014-04-28 22:17:10 -0400

    Fixed error_handling files so no multi-defined compile error

---
+++ error_handling.cu
@@ -0,0 +1,11 @@
+#include "error_handling.h"
+
+void gpuCheckError( cudaError_t err,
+                         const char *file,
+                         int line ) {
+    if (err != cudaSuccess) {
+        printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
+                file, line );
+        exit( EXIT_FAILURE );
+    }
+}
--- error_handling.h
+++ error_handling.h
@@ -6,13 +6,7 @@
 #define GPU_CHECKERROR( err ) (gpuCheckError( err, __FILE__, __LINE__ ))
 void gpuCheckError( cudaError_t err,
                          const char *file,
-                         int line ) {
-    if (err != cudaSuccess) {
-        printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
-                file, line );
-        exit( EXIT_FAILURE );
-    }
-}
+                         int line );
 
 //#define DEBUG
 
